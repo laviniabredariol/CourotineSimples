63e1914620c483c39d09e8b04e3aacc3efe4a7d1
laviniabredariol <laviniabredariol@gmail.com>
2024-05-09 16:53:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/courotinesimples/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.courotinesimples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+data class ExemploModel (
+    val id: Int,
+    val descricao: String
+)
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tvTexto = findViewById<TextView>(R.id.tvTextoMuitoGrande)
+        val pb = findViewById<ProgressBar>(R.id.pb)
+        val btCarregarDados = findViewById<Button>(R.id.btCarregarDados)
+
+        btCarregarDados.setOnClickListener {
+            pb.isVisible = true
+            btCarregarDados.isVisible = false
+
+//escopo para função assíncrona
+//adicionar a implementação no gradle (app)
+//implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+            lifecycleScope.launch {
+                val result = buscarDados()
+                tvTexto.text = result.descricao
+                pb.isVisible = false
+                btCarregarDados.isVisible = true
+            }
+
+        }
+    }
+
+    //função assíncrona, chamar através de coroutine
+    suspend fun buscarDados (): ExemploModel {
+        delay(3000)
+        return ExemploModel(
+            1,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )
+
+    }
+}
